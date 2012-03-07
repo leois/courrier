cc3106ede6719fddd07d8c33265ed4f4e4f6b765
Piul <raulbeni@gmail.com>
2012-03-07 14:42:11 -0300

    creacion del proyecto courrier_project

---
+++ courrier_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourrierProject::Application.config.secret_token = '30c1be2899d63575d422346f9070cf6d8e498c65e07587c8eb36b8fce5bf4f407dd95e8c9ebfd1596687a97f3e412f44f08988a7b57e82c2a25ceef08fb80ba1'
